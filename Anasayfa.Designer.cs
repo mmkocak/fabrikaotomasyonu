26d3470a42a91280bb7f50c71a505688e8cef43a
mmkocak <muhammetlocak774@gmail.com>
2024-06-27 18:23:48 +0300

    anasayfa tasarımı bitti

--- Anasayfa.Designer.cs
+++ Anasayfa.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Anasayfa));
             this.personelBtn = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -39,61 +40,65 @@
             // personelBtn
             // 
             this.personelBtn.BackColor = System.Drawing.Color.DarkBlue;
+            this.personelBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("personelBtn.BackgroundImage")));
+            this.personelBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.personelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.personelBtn.ForeColor = System.Drawing.Color.White;
-            this.personelBtn.Location = new System.Drawing.Point(228, 217);
+            this.personelBtn.Location = new System.Drawing.Point(85, 12);
             this.personelBtn.Name = "personelBtn";
-            this.personelBtn.Size = new System.Drawing.Size(241, 63);
+            this.personelBtn.Size = new System.Drawing.Size(412, 344);
             this.personelBtn.TabIndex = 7;
-            this.personelBtn.Text = "Personel";
             this.personelBtn.UseVisualStyleBackColor = false;
             this.personelBtn.Click += new System.EventHandler(this.personelBtn_Click);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.DarkBlue;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(228, 344);
+            this.button3.Location = new System.Drawing.Point(85, 395);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(241, 63);
+            this.button3.Size = new System.Drawing.Size(412, 390);
             this.button3.TabIndex = 9;
-            this.button3.Text = "Pancar Muhasebe";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(545, 344);
+            this.button1.Location = new System.Drawing.Point(761, 395);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(241, 63);
+            this.button1.Size = new System.Drawing.Size(468, 390);
             this.button1.TabIndex = 12;
-            this.button1.Text = "Stok";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button5
             // 
             this.button5.BackColor = System.Drawing.Color.DarkBlue;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(545, 217);
+            this.button5.Location = new System.Drawing.Point(761, 12);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(241, 63);
+            this.button5.Size = new System.Drawing.Size(468, 344);
             this.button5.TabIndex = 10;
-            this.button5.Text = "Yemekhane";
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::fabrikaotomasyonu.Properties.Resources.pancar2;
-            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1002, 649);
+            this.pictureBox1.Size = new System.Drawing.Size(1342, 810);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -102,7 +107,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.ClientSize = new System.Drawing.Size(1336, 808);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
